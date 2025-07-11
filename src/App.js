bc96575d6edbea617936d5063faa8cae25f2eeee
Yashu-04-ky <yashwanthikummetha@gmail.com>
2025-07-11 12:34:14 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Analytics from "./pages/Analytics";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/analytics/:id" element={<Analytics />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
